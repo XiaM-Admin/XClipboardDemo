66e9a2f2f50eb6167cfa9b8d4eadde98afc209dc
Mr.Xia <1424473282@qq.com>
2023-03-06 22:13:10 +0800

    添加项目文件。

---
+++ XClipboard/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace XClipboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sharpclipboard = new WK.Libraries.SharpClipboardNS.SharpClipboard(this.components);
+            this.ShowTip = new System.Windows.Forms.TextBox();
+            this.ShowPic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.testsqlite = new System.Windows.Forms.Button();
+            this.testsqlite2 = new System.Windows.Forms.Button();
+            this.testsqlite3 = new System.Windows.Forms.Button();
+            this.testsqlite4 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.TxtID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtText = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.testsqlite5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ShowPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sharpclipboard
+            // 
+            this.sharpclipboard.MonitorClipboard = true;
+            this.sharpclipboard.ObservableFormats.All = true;
+            this.sharpclipboard.ObservableFormats.Files = true;
+            this.sharpclipboard.ObservableFormats.Images = true;
+            this.sharpclipboard.ObservableFormats.Others = true;
+            this.sharpclipboard.ObservableFormats.Texts = true;
+            this.sharpclipboard.ObserveLastEntry = true;
+            this.sharpclipboard.Tag = null;
+            // 
+            // ShowTip
+            // 
+            this.ShowTip.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ShowTip.Location = new System.Drawing.Point(12, 12);
+            this.ShowTip.Multiline = true;
+            this.ShowTip.Name = "ShowTip";
+            this.ShowTip.ReadOnly = true;
+            this.ShowTip.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ShowTip.Size = new System.Drawing.Size(286, 296);
+            this.ShowTip.TabIndex = 0;
+            // 
+            // ShowPic
+            // 
+            this.ShowPic.Location = new System.Drawing.Point(304, 12);
+            this.ShowPic.Name = "ShowPic";
+            this.ShowPic.Size = new System.Drawing.Size(249, 216);
+            this.ShowPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ShowPic.TabIndex = 1;
+            this.ShowPic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "这仅是一个使用了SharpClipboard库的示例使用测试程序！";
+            // 
+            // testsqlite
+            // 
+            this.testsqlite.Location = new System.Drawing.Point(460, 234);
+            this.testsqlite.Name = "testsqlite";
+            this.testsqlite.Size = new System.Drawing.Size(93, 23);
+            this.testsqlite.TabIndex = 3;
+            this.testsqlite.Text = "初始化数据库";
+            this.testsqlite.UseVisualStyleBackColor = true;
+            this.testsqlite.Click += new System.EventHandler(this.testsqlite_Click);
+            // 
+            // testsqlite2
+            // 
+            this.testsqlite2.Location = new System.Drawing.Point(460, 292);
+            this.testsqlite2.Name = "testsqlite2";
+            this.testsqlite2.Size = new System.Drawing.Size(93, 23);
+            this.testsqlite2.TabIndex = 4;
+            this.testsqlite2.Text = "写库数据";
+            this.testsqlite2.UseVisualStyleBackColor = true;
+            this.testsqlite2.Click += new System.EventHandler(this.testsqlite2_Click);
+            // 
+            // testsqlite3
+            // 
+            this.testsqlite3.Location = new System.Drawing.Point(460, 318);
+            this.testsqlite3.Name = "testsqlite3";
+            this.testsqlite3.Size = new System.Drawing.Size(93, 23);
+            this.testsqlite3.TabIndex = 5;
+            this.testsqlite3.Text = "读库数据";
+            this.testsqlite3.UseVisualStyleBackColor = true;
+            this.testsqlite3.Click += new System.EventHandler(this.testsqlite3_Click);
+            // 
+            // testsqlite4
+            // 
+            this.testsqlite4.Location = new System.Drawing.Point(361, 263);
+            this.testsqlite4.Name = "testsqlite4";
+            this.testsqlite4.Size = new System.Drawing.Size(93, 23);
+            this.testsqlite4.TabIndex = 6;
+            this.testsqlite4.Text = "硬清空库数据";
+            this.testsqlite4.UseVisualStyleBackColor = true;
+            this.testsqlite4.Click += new System.EventHandler(this.testsqlite4_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TxtID,
+            this.TxtType,
+            this.TxtTime,
+            this.TxtText});
+            this.dataGridView.Location = new System.Drawing.Point(12, 347);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 23;
+            this.dataGridView.Size = new System.Drawing.Size(541, 358);
+            this.dataGridView.TabIndex = 7;
+            this.dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellDoubleClick);
+            // 
+            // TxtID
+            // 
+            this.TxtID.HeaderText = "ID";
+            this.TxtID.Name = "TxtID";
+            this.TxtID.ReadOnly = true;
+            this.TxtID.Width = 30;
+            // 
+            // TxtType
+            // 
+            this.TxtType.HeaderText = "Type";
+            this.TxtType.Name = "TxtType";
+            this.TxtType.ReadOnly = true;
+            this.TxtType.Width = 55;
+            // 
+            // TxtTime
+            // 
+            this.TxtTime.HeaderText = "Time";
+            this.TxtTime.Name = "TxtTime";
+            this.TxtTime.ReadOnly = true;
+            this.TxtTime.Width = 150;
+            // 
+            // TxtText
+            // 
+            this.TxtText.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TxtText.HeaderText = "Text";
+            this.TxtText.Name = "TxtText";
+            this.TxtText.ReadOnly = true;
+            // 
+            // testsqlite5
+            // 
+            this.testsqlite5.Location = new System.Drawing.Point(460, 263);
+            this.testsqlite5.Name = "testsqlite5";
+            this.testsqlite5.Size = new System.Drawing.Size(93, 23);
+            this.testsqlite5.TabIndex = 8;
+            this.testsqlite5.Text = "软清空库数据";
+            this.testsqlite5.UseVisualStyleBackColor = true;
+            this.testsqlite5.Click += new System.EventHandler(this.testsqlite5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 717);
+            this.Controls.Add(this.testsqlite5);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.testsqlite4);
+            this.Controls.Add(this.testsqlite3);
+            this.Controls.Add(this.testsqlite2);
+            this.Controls.Add(this.testsqlite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ShowPic);
+            this.Controls.Add(this.ShowTip);
+            this.Name = "Form1";
+            this.Text = "测试案例";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ShowPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private WK.Libraries.SharpClipboardNS.SharpClipboard sharpclipboard;
+        private System.Windows.Forms.TextBox ShowTip;
+        private System.Windows.Forms.PictureBox ShowPic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button testsqlite;
+        private System.Windows.Forms.Button testsqlite2;
+        private System.Windows.Forms.Button testsqlite3;
+        private System.Windows.Forms.Button testsqlite4;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TxtText;
+        private System.Windows.Forms.Button testsqlite5;
+    }
+}
+
---
+++ XClipboard/Form1.cs
@@ -0,0 +1,380 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XClipboard
+{
+    public partial class Form1 : Form
+    {
+        private Image lasteContent = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 判断图片是否一致
+        /// </summary>
+        /// <param name="img">图片一</param>
+        /// <param name="bmp">图片二</param>
+        /// <returns>是否一致</returns>
+        public static bool IsSameImg(Bitmap img, Bitmap bmp)
+        {
+            if (bmp == null || img == null) return false;
+            //大小一致
+            if (img.Width == bmp.Width && img.Height == bmp.Height)
+            {
+                //将图片一锁定到内存
+                BitmapData imgData_i = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                IntPtr ipr_i = imgData_i.Scan0;
+                int length_i = imgData_i.Width * imgData_i.Height * 3;
+                byte[] imgValue_i = new byte[length_i];
+                Marshal.Copy(ipr_i, imgValue_i, 0, length_i);
+                img.UnlockBits(imgData_i);
+                //将图片二锁定到内存
+                BitmapData imgData_b = img.LockBits(new Rectangle(0, 0, img.Width, img.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                IntPtr ipr_b = imgData_b.Scan0;
+                int length_b = imgData_b.Width * imgData_b.Height * 3;
+                byte[] imgValue_b = new byte[length_b];
+                Marshal.Copy(ipr_b, imgValue_b, 0, length_b);
+                img.UnlockBits(imgData_b);
+                //长度不相同
+                if (length_i != length_b)
+                {
+                    return false;
+                }
+                else
+                {
+                    //循环判断值
+                    for (int i = 0; i < length_i; i++)
+                    {
+                        //不一致
+                        if (imgValue_i[i] != imgValue_b[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            else
+                return false;
+        }
+        /// <summary>
+        /// 连接数据库文件
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        private bool ConnectSqlite(SqliteConnection connection)
+        {
+            try
+            {
+                connection.Open();
+                SqliteCommand cmd = new SqliteCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS AllData(Id INTEGER PRIMARY KEY AUTOINCREMENT,Time TEXT,Fromm TEXT,Type TEXT,Content TEXT)";
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+            }
+            catch (Exception e)
+            {
+                if (connection.ConnectionString.Contains("Password"))
+                    MessageBox.Show("连接数据库失败，可能您的数据库设置了密码，请确保您的密码正确后重试。\r\n" +
+                        e.Message, "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    MessageBox.Show("数据库连接失败，请重试。\r\n" +
+                        e.Message, "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// 窗口关闭后
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            sharpclipboard.StopMonitoring();
+        }
+        /// <summary>
+        /// 窗口加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sharpclipboard.StartMonitoring();
+            sharpclipboard.ClipboardChanged += Sharpclipboard_ClipboardChanged;
+            dataGridView.RowHeadersVisible = false;
+        }
+        /// <summary>
+        /// 打开数据库文件
+        /// </summary>
+        /// <returns></returns>
+        private SqliteConnection OpenSqlite()
+        {
+            Directory.CreateDirectory("Data\\");
+            var connStr = @"Data Source=Data\\TestSqlite.db";//连接字符串
+            var conn = new SqliteConnectionStringBuilder(connStr)
+            {
+                Mode = SqliteOpenMode.ReadWriteCreate,
+                //Password = "password"
+            }.ToString();//使用这个方式设置密码，避免sql注入
+            return new SqliteConnection(conn);//创建SQLite连接
+        }
+        /// <summary>
+        /// 剪贴板监听事件，剪贴板信息改动时进入事件。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Sharpclipboard_ClipboardChanged(object sender, WK.Libraries.SharpClipboardNS.SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            if (e.SourceApplication.Title == this.Text) return; // 不复制自己的内容
+
+            ShowTip.BeginInvoke((MethodInvoker)delegate ()
+            {
+                switch (e.ContentType)
+                {
+                    case WK.Libraries.SharpClipboardNS.SharpClipboard.ContentTypes.Text:
+                        ShowTip.Text = "剪贴板类型：文本类型\r\n\r\n";
+                        ShowTip.Text += sharpclipboard.ClipboardText;
+                        WriteSqlite("文本类型", sharpclipboard.ClipboardText, e.SourceApplication.Title);
+                        break;
+
+                    case WK.Libraries.SharpClipboardNS.SharpClipboard.ContentTypes.Image:
+                        ShowTip.Text = "剪贴板类型：图片类型\r\n\r\n" +
+                        "         =====>>      ";
+                        Image img = sharpclipboard.ClipboardImage;
+                        ShowPic.Image = img;
+                        string picname = "Pic\\" + DateTime.Now.ToString("yyyy MM-dd HH-mm-ss") + ".png";
+                        if (!Directory.Exists("Pic\\")) Directory.CreateDirectory("Pic\\");
+                        using (FileStream f = new FileStream(picname, FileMode.Create))
+                            img.Save(f, ImageFormat.Png);
+
+                        WriteSqlite("图片类型", picname, e.SourceApplication.Title, e.Content);
+                        break;
+
+                    case WK.Libraries.SharpClipboardNS.SharpClipboard.ContentTypes.Files:
+                        ShowTip.Text = "剪贴板类型：文件类型\r\n\r\n";
+                        string txt = "";
+                        if (sharpclipboard.ClipboardFiles.Count > 0)
+                            foreach (var item in sharpclipboard.ClipboardFiles)
+                            {
+                                //获取文件大小 后缀 类型
+                                FileInfo fileInfo = new FileInfo(item);
+                                if (fileInfo.Attributes == FileAttributes.Directory)
+                                {
+                                    ShowTip.Text += "文件夹名：" + fileInfo.Name + "\r\n";
+                                    ShowTip.Text += "文件夹路径：" + item + "\r\n\r\n";
+                                }
+                                else
+                                {
+                                    ShowTip.Text += "文件名：" + fileInfo.Name + "\r\n";
+                                    ShowTip.Text += "文件大小：" + fileInfo.Length + " KB\r\n";
+                                    ShowTip.Text += "文件路径：" + item + "\r\n\r\n";
+                                }
+                                txt += item + "\r\n";
+                            }
+                        WriteSqlite("文件类型", txt, e.SourceApplication.Title);
+                        break;
+
+                    default:
+                        ShowTip.Text = "剪贴板类型：未知\r\n\r\n";
+                        break;
+                }
+            });
+        }
+        /// <summary>
+        /// 初始化数据库
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testsqlite_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var connection = OpenSqlite();
+                // 创建一个表 记录 序号（主键、自增）时间（文本）类型内容(文本)
+                if (connection.State != ConnectionState.Open)
+                {
+                    if (!ConnectSqlite(connection)) return;
+                    SqliteCommand cmd = new SqliteCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS AllData(Id INTEGER PRIMARY KEY AUTOINCREMENT,Time TEXT,Fromm TEXT,Type TEXT,Content TEXT)";
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+                Console.WriteLine("初始化数据库完毕");
+            });
+        }
+        /// <summary>
+        /// 写数据库
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testsqlite2_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var connection = OpenSqlite();
+                if (connection.State != ConnectionState.Open)
+                {
+                    if (!ConnectSqlite(connection)) return;
+                    SqliteCommand cmd = new SqliteCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "INSERT INTO AllData(Time,Fromm,Type,Content) VALUES(@Time,@Fromm,@Type,@Content)";
+                    cmd.Parameters.Add("@Time", SqliteType.Text).Value = DateTime.Now.ToString();
+                    cmd.Parameters.Add("@Type", SqliteType.Text).Value = "文本类型";
+                    cmd.Parameters.Add("@Content", SqliteType.Text).Value = $"测试信息";
+                    cmd.Parameters.Add("@Fromm", SqliteType.Text).Value = $"测试程序";
+
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            });
+        }
+        /// <summary>
+        /// 读取数据库，显示到表格中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testsqlite3_Click(object sender, EventArgs e)
+        {
+            Task t = Task.Run(() =>
+            {
+                //读数据库
+                var connection = OpenSqlite();
+                if (connection.State != ConnectionState.Open)
+                {
+                    if (!ConnectSqlite(connection)) return;
+                    SqliteCommand cmd = new SqliteCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT * FROM AllData";
+                    SqliteDataReader reader = cmd.ExecuteReader();
+                    dataGridView.Invoke(new Action(() =>
+                    {
+                        dataGridView.Rows.Clear();
+                    }));
+
+                    while (reader.Read())
+                    {
+                        ShowTip.Invoke(new Action(() =>
+                        {
+                            ShowTip.Text += "序号：" + reader["Id"] + "\r\n";
+                            ShowTip.Text += "时间：" + reader["Time"] + "\r\n";
+                            ShowTip.Text += "类型：" + reader["Type"] + "\r\n";
+                            ShowTip.Text += "内容：" + reader["Content"] + "\r\n\r\n";
+                        }));
+                        dataGridView.Invoke(new Action(() =>
+                        {
+                            int index = this.dataGridView.Rows.Add();
+                            dataGridView.Rows[index].Cells[0].Value = reader["Id"];
+                            dataGridView.Rows[index].Cells[1].Value = reader["Type"];
+                            dataGridView.Rows[index].Cells[2].Value = reader["Time"];
+                            dataGridView.Rows[index].Cells[3].Value = reader["Content"];
+                        }));
+                    }
+                }
+                connection.Close();
+            });
+        }
+        /// <summary>
+        /// 硬删除数据库数据，删除db文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testsqlite4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("Data\\TestSqlite.db"))
+            {
+                try
+                {
+                    File.Delete("Data\\TestSqlite.db");
+                }
+                catch
+                {
+                    MessageBox.Show("数据库被占用，请重启程序后重试", "通知", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            if (!File.Exists("Data\\TestSqlite.db"))
+            {
+                MessageBox.Show("数据库成功删除", "通知", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                testsqlite_Click(null, null);//初始化一个空的数据库
+            }
+        }
+        /// <summary>
+        /// 软清空数据库数据，清空库
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void testsqlite5_Click(object sender, EventArgs e)
+        {
+            var connection = OpenSqlite();
+            //清空数据库内容
+            if (connection.State != ConnectionState.Open)
+            {
+                if (!ConnectSqlite(connection)) return;
+                SqliteCommand cmd = new SqliteCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "delete from AllData";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "update sqlite_sequence SET seq = 0 where name ='AllData'";
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            MessageBox.Show("数据库表已清空", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// 写数据库数据
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="text"></param>
+        /// <param name="From"></param>
+        /// <param name="eContent"></param>
+        private void WriteSqlite(string type, string text, string From, object eContent = null)
+        {
+            if (eContent != null)
+            {
+                if (IsSameImg((Bitmap)eContent, (Bitmap)lasteContent)) return;
+                lasteContent = (Image)eContent;
+            }
+            Task.Run(() =>
+            {
+                var connection = OpenSqlite();
+                if (connection.State != ConnectionState.Open)
+                {
+                    if (!ConnectSqlite(connection)) return;
+                    SqliteCommand cmd = new SqliteCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "INSERT INTO AllData(Time,Fromm,Type,Content) VALUES(@Time,@Fromm,@Type,@Content)";
+                    cmd.Parameters.Add("@Time", SqliteType.Text).Value = DateTime.Now.ToString();
+                    cmd.Parameters.Add("@Type", SqliteType.Text).Value = type;
+                    cmd.Parameters.Add("@Content", SqliteType.Text).Value = text;
+                    cmd.Parameters.Add("@Fromm", SqliteType.Text).Value = From;
+                    cmd.ExecuteNonQuery();
+                }
+                connection.Close();
+            });
+        }
+
+        private void dataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3 && dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString() == "图片类型") //第三列 剪贴板内容列
+            {
+                //打开图片
+                Process.Start(dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString());
+            }
+        }
+
+    }
+}
